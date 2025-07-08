1f3f2c40d13a0e4a3ebf647428b2c8170739e4b1
lemonca <80398419+lemonca11@users.noreply.github.com>
2025-07-08 18:19:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,21 +17,21 @@ def index():
             start_date = datetime.strptime(date_str, '%Y-%m-%d')
             end_date = start_date + timedelta(days=1)
         except ValueError:
-            return "  YYYY-MM-DD"
+            return "❌ 日期格式不正确，应为 YYYY-MM-DD"
 
         output_files = []
         os.makedirs("data", exist_ok=True)
 
         for ticker in tickers:
-            print(f" {ticker} ...")
+            print(f"正在下载 {ticker} 的数据...")
 
             df = yf.download(ticker, start=start_date, end=end_date, interval="1m", prepost=False)
 
             if df.empty:
-                print(f"{ticker} ")
+                print(f"{ticker} 没有数据，跳过。")
                 continue
 
-            df[""] = df["Close"] * df["Volume"]
+            df["成交额"] = df["Close"] * df["Volume"]
             df.index = df.index.tz_convert("America/New_York")
             df = df.between_time("09:30", "16:00")
             df.index = df.index.tz_localize(None)
@@ -48,4 +48,3 @@ def index():
 def download_file(filename):
     file_path = os.path.join('data', filename)
     return send_file(file_path, as_attachment=True)
-PK
